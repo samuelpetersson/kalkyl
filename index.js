d888489d72163b1ed2051e62e9829a1082b73d84
Samuel Petersson <samuelpetersson@pm.me>
2020-11-16 15:03:38 +0100

    Require calculation

--- index.js
+++ index.js
@@ -60,41 +60,24 @@ var kalkyl = (function() {
 		return true
 	}
 
-	var place = (into, path, value) => {
-		var comps = path.split('.')
-		var field = comps.pop()
-		while(comps.length > 0) {
-			var cur = comps.shift()
-			into = into[cur] || (into[cur] = {})
-		}
-		into[field] = value
-	}
-
-	var expand = function(object) {
-		var result = {}
-		for(var name in object) {
-			place(result, name, object[name])
-		}
-		return result
-	}
-
 	return function() {
 
 		var calcs = {}
 		var views = []
 
 		var prepare = function(args, value, result) {
-			value = expand(value)
 			for(var name in value) {
 				var calc = calcs[name]
-				if(calc) {
-					if(value[name] != null && calc.remap) {
-						result = prepare(args, calc.remap(value[name]), result)
-					}
-					else {
-						args[name] = value[name]
-						result = true
-					}
+				if(!calc) {
+					calc = {solve: function(data, args, prev) { return args !== undefined ? args : prev }}
+					calcs[name] = calc
+				}
+				if(value[name] != null && calc.remap) {
+					result = prepare(args, calc.remap(value[name]), result)
+				}
+				else {
+					args[name] = value[name]
+					result = true
 				}
 			}
 			return result == true
@@ -110,7 +93,7 @@ var kalkyl = (function() {
 			var calc = calcs[name]
 			var flag = context.flag
 
-			if(!calc || !calc.solve) {
+			if(!calc) {
 				flag[name] = false
 			}
 
@@ -156,21 +139,17 @@ var kalkyl = (function() {
 				var args = {}
 
 				if(value && !prepare(args, value)) {
-					return state.data
+					return
 				}
 
 				var context = {data:clone(state.data || {}), diff:{}, args:args, flag:{}}
 				var queue = Object.keys(args)
 				
 				if(queue.length > 0 && !resolve(context, queue)) {
-					return state.data
+					return
 				}
 
 				resolve(context, Object.keys(calcs))
-
-				if(Object.keys(context.diff) == 0) {
-					return state.data
-				}
 
 				var data = clone(context.data)
 				
